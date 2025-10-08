86b915723df8ee69be119cf35631b4cd0a404b97
Johan Burke <jburke.create@gmail.com>
2025-10-08 16:03:46 -0400

    feat: add lspci

--- modules/programs/general-utils.nix
+++ modules/programs/general-utils.nix
@@ -7,5 +7,8 @@ delib.module {
   name = "programs.general-utils";
 
   options = delib.singleEnableOption true;
-  nixos.ifEnabled.environment.systemPackages = with pkgs; [ htop ];
+  nixos.ifEnabled.environment.systemPackages = with pkgs; [
+    htop
+    pciutils
+  ];
 }
