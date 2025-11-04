611be191af4ea6e5272ff5b365a2f4585d08efe4
Johan Burke <jburke.create@gmail.com>
2025-11-04 16:53:20 -0500

    fix: add missing ssh config on laptop

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -3,6 +3,16 @@ delib.host {
   name = "laptop";
 
   type = "laptop";
-  myconfig.programs.network-utils.enable = true;
+  myconfig.programs = {
+    network-utils.enable = true;
+    ssh = {
+      keyConfigs = [
+        {
+          host = "github.com";
+          identityFileSuffix = "github";
+        }
+      ];
+    };
+  };
   rice = "dark";
 }
