a62c0930d2edf833b457ea5292ac0cc570b1fb02
Johan Burke <jburke.create@gmail.com>
2025-11-07 21:09:39 -0500

    fix: add improved git settings and delta pager

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -7,6 +7,25 @@ delib.module {
   home.ifEnabled =
     { myconfig, ... }:
     {
+      programs.delta = {
+        enable = true;
+        enableGitIntegration = true;
+        options = {
+          line-numbers = true;
+          # styles from https://github.com/folke/tokyonight.nvim/blob/main/extras/delta/tokyonight_storm.gitconfig
+          minus-style = "syntax #52313f";
+          minus-non-emph-style = "syntax #52313f";
+          minus-emph-style = "syntax #763842";
+          minus-empty-line-marker-style = "syntax #52313f";
+          line-numbers-minus-style = "#914c54";
+          plus-style = "syntax #2b485a";
+          plus-non-emph-style = "syntax #2b485a";
+          plus-emph-style = "syntax #316172";
+          plus-empty-line-marker-style = "syntax #2b485a";
+          line-numbers-plus-style = "#449dab";
+          line-numbers-zero-style = "#3b4261";
+        };
+      };
       programs.git = {
         enable = true;
         lfs.enable = true;
@@ -18,7 +37,21 @@ delib.module {
           };
           credential.helper = "store";
           init.defaultBranch = "main";
-          push.autoSetupRemote = true;
+          push = {
+            autoSetupRemote = true;
+            followTags = true;
+          };
+          diff = {
+            colorMoved = "zebra";
+            algorithm = "patience";
+          };
+          color = {
+            ui = "auto";
+            status = "auto";
+            branch = "auto";
+          };
+          rerere.enabled = true; # remember conflict resolutions for future merges
+          fetch.prune = true;
         };
       };
     };
