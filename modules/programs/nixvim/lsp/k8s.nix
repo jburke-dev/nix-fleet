985606ec56bf09b09b6c15392775fe6e901b0cb1
Johan Burke <jburke.create@gmail.com>
2025-11-12 15:32:06 -0500

    feat: add k8s editor support

--- modules/programs/nixvim/lsp/k8s.nix
+++ modules/programs/nixvim/lsp/k8s.nix
@@ -1,12 +1,13 @@
 { delib, ... }:
 delib.module {
-  name = "programs.nixvim.lsp.yamlls";
+  name = "programs.nixvim.lsp.k8s";
 
   options = delib.singleCascadeEnableOption;
 
   home.ifEnabled = {
     programs.nixvim.lsp.servers = {
       yamlls.enable = true;
+      helm_ls.enable = true;
     };
   };
 }
